a75ac78f9ad01170d0d85de6d3917939939bc1d1
cshou <cshou@google.com>
2021-10-01 23:30:34 -0700

    update tf output

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,5 +58,5 @@ resource "google_service_account_iam_member" "pool_impersonation" {
 }
 
 output "workload_identity_pool_id" {
-  value = google_iam_workload_identity_pool.github_pool.name
+  value = google_iam_workload_identity_pool_provider.github_provider.name
 }
